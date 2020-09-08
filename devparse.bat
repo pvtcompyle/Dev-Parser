6397c783c7d57591eb7e9427067455f0e768c00a
pvt_compyle <43185139+pvtcompyle@users.noreply.github.com>
2020-09-07 20:43:09 -0500

    Update devparse.bat

--- devparse.bat
+++ devparse.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
 rem Change this directory to the location of your devparse.py file
-python E:\GitHub\Dev-Parser\devparse.py -p %1
+python R:\GitHub\Dev-Parser\devparse.py -p %1
 timeout 30
